9579870064dbd6543ea64838b6e35f853830f82a
AnderMB <andermarinb@gmail.com>
2023-02-06 12:24:02 +0100

    update 0.4 & gradle 0.3

--- app/src/main/java/com/example/ander/views/giftL/comm/GiftListItem.kt
+++ app/src/main/java/com/example/ander/views/giftL/comm/GiftListItem.kt
@@ -14,10 +14,12 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.ander.R
 import com.example.ander.gifts.Gift
 
+@OptIn(ExperimentalCoilApi::class)
 @ExperimentalMaterialApi
 @Composable
 fun GiftListItem(
